0e46f5996504316e9c466f5970cffe135051e804
emilioSp <emilio.spatola@gmail.com>
2021-02-04 19:53:15 +0100

    arghhhh

--- index.js
+++ index.js
@@ -5,34 +5,37 @@ const { execSync } = require('child_process');
 
 console.log('c9aooo');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const message = core.getInput('message');
-  const ghToken = core.getInput('GITHUB_TOKEN');
+(async() => {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const message = core.getInput('message');
+    const ghToken = core.getInput('GITHUB_TOKEN');
 
-  const context = github.context;
-  if (context.payload.pull_request == null) {
-    core.setFailed('No pull request found.');
-    return;
+    const context = github.context;
+    if (context.payload.pull_request == null) {
+      core.setFailed('No pull request found.');
+      return;
+    }
+    const prNumber = context.payload.pull_request.number;
+
+    const octokit = new Octokit({
+      auth: ghToken
+    });
+
+    // Create a new Comment
+    const c  = await octokit.issues.createComment({
+      ...context.repo,
+      issue_number: prNumber,
+      body: message
+    });
+
+    console.log('comment', c);
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
   }
-  const prNumber = context.payload.pull_request.number;
 
-  const octokit = new Octokit({
-    auth: ghToken
-  });
-
-  // Create a new Comment
-  const c  = await octokit.issues.createComment({
-    ...context.repo,
-    issue_number: prNumber,
-    body: message
-  });
-
-  console.log('comment', c);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+})();
