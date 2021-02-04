9eb66ddb34d5586f88444238c2780375d7d02a7f
Emilio Spatola <emilio.spatola@gmail.com>
2021-02-04 20:14:15 +0100

    test (#1)

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const { context } = require('@actions/github');
 const { Octokit } = require('@octokit/rest');
 const { execSync } = require('child_process');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const message = core.getInput('message');
-  const ghToken = core.getInput('GITHUB_TOKEN');
+(async() => {
+  try {
+    const command = core.getInput('command');
+    const ghToken = core.getInput('GITHUB_TOKEN');
 
-  const context = github.context;
-  if (context.payload.pull_request == null) {
-    core.setFailed('No pull request found.');
-    return;
+    if (context.payload.pull_request == null) {
+      core.setFailed('No pull request found.');
+      return;
+    }
+
+    const results = execSync(command).toString();
+
+    const octokit = new Octokit({
+      auth: ghToken
+    });
+
+    // Create a new Comment
+    await octokit.issues.createComment({
+      ...context.repo,
+      issue_number: context.payload.pull_request.number,
+      body: results
+    });
+  } catch (error) {
+    core.setFailed(error.message);
   }
-  const prNumber = context.payload.pull_request.number;
 
-  const octokit = new Octokit({
-    auth: ghToken
-  });
-
-  // Create a new Comment
-  const c  = await octokit.issues.createComment({
-    ...context.repo,
-    issue_number: prNumber,
-    body: message
-  });
-
-  console.log('comment', c);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+})();
