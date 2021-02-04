5d6775e79bab41209a8048b39e2ec078303310a0
emilioSp <emilio.spatola@gmail.com>
2021-02-04 19:37:17 +0100

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
-  const ls = execSync('ls').toString();
+  const ls = execSync('date').toString();
   console.log(ls);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
